68d5edd92f9912c41098cbcba5933ac8f9497274
Benjamin Odisho <Semiroundpizza8@gmail.com>
2018-02-14 18:10:42 -0600

    Created Word Def Component

--- src/js/popup/greeting_component.jsx
+++ src/js/popup/greeting_component.jsx
@@ -16,40 +16,29 @@ export default class Main extends React.Component {
     super(props)
     this.state = {
       word: 'dog',
-      definition: [],
       audioData: []
     }
   }
   componentDidMount() {
     let wordnikStart = 'http://api.wordnik.com:80/v4/word.json';
-    let definitionEnd = `definitions?limit=1&includeRelated=false&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=${secrets.WORDNIK_API_KEY}`;
     let audioEnd = `audio?useCanonical=false&limit=50&api_key=${secrets.WORDNIK_API_KEY}`;
     let word = this.state.word;
-
-    // Request for Definition
-    axios.get(`${wordnikStart}/${word}/${definitionEnd}`)
-      .then(toJson)
-      .then(item => {
-        console.log('wordData', item.data);
-        this.setState({ definition: item.data });
-      });
 
     // Request for audio
     axios.get(`${wordnikStart}/${word}/${audioEnd}`)
       .then(toJson)
       .then(item => {
-        console.log('audio', item.data);
         this.setState({ audioData: item.data });
       });
   }
   render() {
     let wordData = this.state.definition;
     let audioData = this.state.audioData;
-    if (!wordData.length || !audioData.length) return (<div />);
+    if (!audioData.length) return (<div />);
     return (
       <div>
         <ImageView />
-        <wordView />
+        <WordView />
         <audio controls>
           <source src={this.state.audioData[0].fileUrl} />
         </audio>
--- src/js/popup/wordView.jsx
+++ src/js/popup/wordView.jsx
@@ -1,17 +1,36 @@
 import React from "react";
+import { toJson } from 'unsplash-js';
+import secrets from './secrets.js';
+import axios from 'axios';
 
-let renderCount = 0;
 export default class ImageView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      definition: [],
+    };
+  }
+
+  componentDidMount() {
+    // Request for Definition
+    let wordnikStart = 'http://api.wordnik.com:80/v4/word.json';
+    let definitionEnd = `definitions?limit=1&includeRelated=false&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=${secrets.WORDNIK_API_KEY}`;
+    let word = 'dog';
+
+    axios.get(`${wordnikStart}/${word}/${definitionEnd}`)
+      .then(toJson)
+      .then(item => {
+        this.setState({ definition: item.data });
+      });
   }
 
   render() {
+    console.log("Def", this.state.definition);
+    if (!this.state.definition.length) return (<div />)
     return (
       <div>
         <h2>Hello</h2>
-        <p>Word Text Here</p>
+        <p>{this.state.definition[0].text}</p>
       </div>
     )
   }
